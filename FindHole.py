640a73af077918485987c17f16121a0e0dcae4bd
EhsanMrh <ehsanmirhashemi41@gmail.com>
2020-11-27 18:51:13 +0330

    Add ip changer

---
+++ FindHole.py
@@ -0,0 +1,41 @@
+import requests
+import time
+from stem import Signal
+from stem.control import Controller
+
+
+def get_current_ip():
+    session = requests.session()
+
+    # TO Request URL with SOCKS over TOR
+    session.proxies = {}
+    session.proxies['http'] = 'socks5h://localhost:9050'
+    session.proxies['https'] = 'socks5h://localhost:9050'
+
+    try:
+        r = session.get('http://httpbin.org/ip')
+
+    except Exception as e:
+        print(str(e))
+    else:
+        return r.text
+
+
+def renew_tor_ip():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate(password="abc123")
+        controller.signal(Signal.NEWNYM)
+
+
+def ip_changer():
+    res = get_current_ip()
+    print("IP change to: ", res)
+    renew_tor_ip()
+
+
+# if __name__ == "__main__":
+#     for i in range(5):
+#         res = get_current_ip()
+#         print(res)
+#         renew_tor_ip()
+#         time.sleep(5)
